50959a1444ef00a3a745e8a49f18c7500e2f03a1
Dmitry Petrov <dima.krutoo@yandex.ru>
2021-07-27 11:58:49 +0500

    #3
    - обновлены линтеры
    - добавлен пакет для работы с переменными окружения
    - задействована обязятельная переменная окружения
    - дописан readme
    - обновлен конфиг CI публикации

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
-const pacakge = require('./package.json');
+const { Env } = require('@humanwhocodes/env');
+const packageInfo = require('./package.json');
 
-const NAME = 'simaLandVendors';
+const { VENDOR_PACKAGE_BUILD_VERSION: buildVersion } = new Env().required;
 
 module.exports = {
   mode: 'production',
   target: 'web',
-  entry: Object.keys(pacakge.dependencies),
+  entry: Object.keys(packageInfo.dependencies),
   performance: {
     maxAssetSize: 400000,
     maxEntrypointSize: 400000,
   },
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    library: NAME,
     filename: 'dependencies.js',
   },
   plugins: [
     new webpack.DllPlugin({
-      name: NAME,
+      name: `simaLandVendors@${buildVersion}`,
       path: path.resolve(__dirname, './dist/manifest.json'),
       format: true,
       entryOnly: true,
